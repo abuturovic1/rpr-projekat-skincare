8b8228b5c522169bd08df7f79f27374f50dfa913
Amina <abuturovic1@etf.unsa.ba>
2023-08-21 16:48:39 +0200

    getAll

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -13,31 +13,69 @@ public class App
     private static ReservationImpl reservationdao;
 
     private static TreatmentImpl treatmentdao;
+
+    private static Scanner ulaz;
     public static void main( String[] args ) throws SQLException {
 
         customerdao = CustomerImpl.getInstance();
         reservationdao = ReservationImpl.getInstance();
         treatmentdao = TreatmentImpl.getInstance();
+        ulaz = new Scanner(System.in);
 
-
+        //Standardni meni
         int opcija = 0;
-        do{
+        do {
             System.out.println("Unesite opciju:\n1 - pretraga\n2 - unos\n3 - izmjena\n4 -brisanje\n0 - kraj programa ");
-            Scanner ulaz = new Scanner(System.in);
+
             opcija = ulaz.nextInt();
-            switch(opcija){
-                case 1: pretraga(); break;
-                case 2: unos(); break;
-                case 3: izmjena(); break;
-                case 4: brisanje(); break;
-                case 0: break;
+            switch (opcija) {
+                case 1:
+                    pretraga();
+                    break;
+                case 2:
+                    unos();
+                    break;
+                case 3:
+                    izmjena();
+                    break;
+                case 4:
+                    brisanje();
+                    break;
+                case 0:
+                    break;
                 default:
                     System.out.println("Nepoznata opcija");
 
             }
-        } while(opcija!=0);
+        } while (opcija != 0);
 
 
-}
+    }
+
+    private static void unos() {
+        String idcustomer,korIme,lozinka,ime,prezime,mail,brTel;
+        System.out.println("Unesite id korisnika: "); //korisnik ne treba da unosi id??
+        idcustomer = ulaz.nextLine();
+        System.out.println("Unesite username korisnika: ");
+        korIme = ulaz.nextLine();
+        System.out.println("Unesite lozinku korisnika: "); //ovo ne bi trebalo al ok ako je admin neki nesto
+        lozinka = ulaz.nextLine();
+        System.out.println("Unesite ime korisnika: ");
+        ime = ulaz.nextLine();
+        System.out.println("Unesite prezime korisnika: ");
+        prezime = ulaz.nextLine();
+        System.out.println("Unesite e-mail korisnika: ");
+        mail = ulaz.nextLine();
+        System.out.println("Unesite broj telefona korisnika: ");
+        brTel = ulaz.nextLine();
+
+    }
+
+
+    private static void pretragaSvih(){
+        for(Customer customer : customerdao.getAll())
+            System.out.println("Imena i prezimena korisnika su: " +customer.getFirstName()+ " " +customer.getLastName());
+
+    }
 }
 
--- src/main/java/ba/unsa/etf/rpr/CustomerImpl.java
+++ src/main/java/ba/unsa/etf/rpr/CustomerImpl.java
@@ -6,8 +6,21 @@ import java.util.List;
 
 public class CustomerImpl implements CustomerDAO{
     private Connection connection;
-    private PreparedStatement ps;
+    private PreparedStatement ps_pretraziSve,ps_dodaj;
     private static CustomerImpl instance = null;
+
+    private CustomerImpl() throws SQLException{
+        String url = "jdbc:mysql://sql.freedb.tech:3306/freedb_rpr baza";
+        String username="freedb_abuturovic1";
+        String pass = System.getenv("DB_PASSWORD");
+        connection = DriverManager.getConnection(url,username,pass);
+        ps_pretraziSve = connection.prepareStatement("SELECT * FROM Customer");
+
+
+    }
+
+
+
  public static CustomerImpl getInstance()throws SQLException{
      if(instance == null) instance = new CustomerImpl();
      return instance;
@@ -20,6 +33,8 @@ public class CustomerImpl implements CustomerDAO{
 
     @Override
     public ArrayList<Customer> get(int id) {
+
+        //ArrayList<Customer> customers = new ArrayList<Customer>();
         return null;
     }
 
@@ -48,7 +63,19 @@ public class CustomerImpl implements CustomerDAO{
         return customer;*/
     @Override
     public List<Customer> getAll() {
-        return null;
+        ArrayList<Customer>customers = new ArrayList<Customer>();
+        try{
+            ResultSet rs = ps_pretraziSve.executeQuery();
+            while(rs.next()){
+                customers.add(new Customer(rs.getInt(1),rs.getString(2),rs.getString(3),
+                        rs.getString(4),rs.getString(5),
+                        rs.getString(6),rs.getString(7)));
+
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return customers;
     }
 
     @Override
@@ -71,4 +98,8 @@ public class CustomerImpl implements CustomerDAO{
         return null;
 
     }
+
+
+
+
 }
