ca0f74d2709c4f5f7aa94cf40356383ee5d114a7
Amina <abuturovic1@etf.unsa.ba>
2023-08-21 14:29:42 +0200

    update commit

--- src/main/java/ba/unsa/etf/rpr/CustomerDAOImpl.java
+++ src/main/java/ba/unsa/etf/rpr/CustomerDAOImpl.java
@@ -2,8 +2,14 @@ package ba.unsa.etf.rpr;
 
 import java.sql.*;
 import java.util.List;
-import java.util.Properties;
+
 public class CustomerDAOImpl implements CustomerDAO{
+    private static CustomerDAOImpl instance = null;
+ public static CustomerDAOImpl getInstance()throws SQLException{
+     if(instance == null) instance = new CustomerDAOImpl();
+     return instance;
+ }
+
     @Override
     public Customer get(int id) throws SQLException{
     Customer customer = null;
@@ -41,8 +47,49 @@ public class CustomerDAOImpl implements CustomerDAO{
     }
 
     @Override
-    public void update(Customer customer) {
+    public int insert(Customer customer) throws SQLException {
+        /*Connection connection;
+        String url = "jdbc:mysql://sql.freedb.tech:3306/freedb_rpr baza";
+        String username="freedb_abuturovic1";
+        String pass = System.getenv("DB_PASSWORD");
+        connection = DriverManager.getConnection(url,username,pass);
+        String sql = "INSERT INTO Customer (customer_id, username, password, first_name, last_name, email, phone_number) VALUES (?,?,?,?,?,?,?)";
+        PreparedStatement ps = connection.prepareStatement(sql);
+        ps.setInt(1,customer.getCustomerID());
+        ps.setString(2, customer.getUsername());
+        ps.setString(3, customer.getPassword());
+        ps.setString(4, customer.getFirstName());
+        ps.setString(5,customer.getLastName());
+        ps.setString(6, customer.getEmail());
+        ps.setString(7, customer.getPhoneNumber());
 
+        int res = ps.executeUpdate();
+
+        return res;
+
+*/
+        return 0;
+    }
+
+    @Override
+    public int update(Customer customer) throws SQLException {
+        Connection connection;
+        String url = "jdbc:mysql://sql.freedb.tech:3306/freedb_rpr baza";
+        String username="freedb_abuturovic1";
+        String pass = System.getenv("DB_PASSWORD");
+        connection = DriverManager.getConnection(url,username,pass);
+        String sql = "UPDATE Customer set customer_id=?, username=?, password=?, first_name=?, last_name=?, email=?, phone_number=? where id=?";
+        PreparedStatement ps = connection.prepareStatement(sql);
+        ps.setInt(1,customer.getCustomerID());
+        ps.setString(2, customer.getUsername());
+        ps.setString(3, customer.getPassword());
+        ps.setString(4,customer.getFirstName());
+        ps.setString(5, customer.getLastName());
+        ps.setString(6, customer.getEmail());
+        ps.setString(7,customer.getPhoneNumber());
+        int res = ps.executeUpdate();
+
+        return res;
     }
 
     @Override
