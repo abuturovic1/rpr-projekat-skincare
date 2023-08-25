38d4872a4c2dda2c404ae8b239c3eb54ab512936
Amina <abuturovic1@etf.unsa.ba>
2023-08-25 23:41:27 +0200

    created a method loginAccount

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/LogInController.java
@@ -0,0 +1,81 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
+import javafx.scene.layout.AnchorPane;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.sql.*;
+import java.util.ResourceBundle;
+
+public class LogInController implements Initializable {
+    @FXML
+    private AnchorPane loginform;
+
+    @FXML
+    private Button si_logInBtn;
+
+    @FXML
+    private PasswordField si_password;
+
+    @FXML
+    private Button si_register;
+
+    @FXML
+    private TextField si_username;
+
+
+    private Connection connection;
+    private PreparedStatement ps;
+    private ResultSet rs;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    public void loginAccount() throws SQLException {
+        String url = "jdbc:mysql://sql.freedb.tech:3306/freedb_rpr baza";
+        String username="freedb_abuturovic1";
+        String pass = System.getenv("DB_PASSWORD");
+        connection = DriverManager.getConnection(url,username,pass);
+        String sql = "SELECT FROM Customer WHERE username = ? AND password = ? ";
+        try{
+            Alert alert;
+            if(si_username.getText().isEmpty() || si_password.getText().isEmpty()){
+                alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error Message");
+                alert.setHeaderText(null);
+                alert.setContentText("Please fill all the blank fields");
+                alert.showAndWait();
+            } else{
+        ps = connection.prepareStatement(sql);
+            ps.setString(1,si_username.getText());
+            ps.setString(2,si_password.getText());
+            rs = ps.executeQuery();
+
+            if(rs.next()){
+                alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Information Message");
+                alert.setHeaderText(null);
+                alert.setContentText("Succesfull LogIn!");
+                alert.showAndWait();
+            }
+            else{
+                alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error Message");
+                alert.setHeaderText(null);
+                alert.setContentText("Incorrect username/password");
+                alert.showAndWait();
+            }
+
+    }} catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
