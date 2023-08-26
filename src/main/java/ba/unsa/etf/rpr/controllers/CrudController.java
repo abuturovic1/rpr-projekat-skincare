12b86fa50321826f3245082da68380db0721ec48
Amina <abuturovic1@etf.unsa.ba>
2023-08-27 00:41:55 +0200

    added CrudController class

--- src/main/java/ba/unsa/etf/rpr/controllers/CrudController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/CrudController.java
@@ -1,5 +1,38 @@
 package ba.unsa.etf.rpr.controllers;
-
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
+import javafx.scene.layout.AnchorPane;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 public class CrudController {
+    @FXML
+    private Button crud_addBtn;
+
+    @FXML
+    private TextField crud_customerID;
+
+    @FXML
+    private Button crud_deleteBtn;
+
+    @FXML
+    private TextField crud_email;
+
+    @FXML
+    private TextField crud_firstname;
+
+    @FXML
+    private TextField crud_lastname;
+
+    @FXML
+    private TextField crud_phone;
+
+    @FXML
+    private Button crud_updateBtn;
+
+    @FXML
+    private TextField crud_username;
 
 }
