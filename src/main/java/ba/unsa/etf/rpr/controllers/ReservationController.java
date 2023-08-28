7a67d3bd9ec3317b07a881bc5d1130768398aa7b
Amina <abuturovic1@etf.unsa.ba>
2023-08-28 14:15:19 +0200

    made a ReservationController class

--- src/main/java/ba/unsa/etf/rpr/controllers/ReservationController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/ReservationController.java
@@ -1,5 +1,30 @@
 package ba.unsa.etf.rpr.controllers;
+import ba.unsa.etf.rpr.ReservationImpl;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import java.sql.SQLException;
+import java.util.List;
+import ba.unsa.etf.rpr.Reservation;
 
 public class ReservationController {
+    @FXML
+    private TextField customerIDField;
+
+    @FXML
+    private ComboBox<String> treatmentComboBox;
+
+    @FXML
+    private DatePicker reservationDatepicker;
+
+    @FXML
+    private TextField timeField;
+
+    @FXML
+    private Button reserveButton;
+
+    private ReservationImpl reservationDAO;
+
 
 }
+
+
