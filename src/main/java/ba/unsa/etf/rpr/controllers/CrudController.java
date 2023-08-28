1061e9a8978d016d250b706fddae0868160c639e
Amina <abuturovic1@etf.unsa.ba>
2023-08-28 12:48:02 +0200

    changed some methods

--- src/main/java/ba/unsa/etf/rpr/controllers/CrudController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/CrudController.java
@@ -1,15 +1,42 @@
 package ba.unsa.etf.rpr.controllers;
+import ba.unsa.etf.rpr.Customer;
+import ba.unsa.etf.rpr.CustomerImpl;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
-import javafx.scene.control.PasswordField;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.event.ActionEvent;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-public class CrudController {
+
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class CrudController implements Initializable{
     @FXML
     private Button crud_addBtn;
+
+    @FXML
+    private TableColumn<?, ?> crud_col_email;
+
+    @FXML
+    private TableColumn<?, ?> crud_col_firstname;
+
+    @FXML
+    private TableColumn<?, ?> crud_col_id;
+
+    @FXML
+    private TableColumn<?, ?> crud_col_lastname;
+
+    @FXML
+    private TableColumn<?, ?> crud_col_phone;
+
+    @FXML
+    private TableColumn<?, ?> crud_col_username;
 
     @FXML
     private TextField crud_customerID;
@@ -35,4 +62,23 @@ public class CrudController {
     @FXML
     private TextField crud_username;
 
+    Connection connection;
+    public ObservableList<Customer> customerList() throws SQLException {
+        ObservableList<Customer>rezultat = FXCollections.observableArrayList();
+        String url = "jdbc:mysql://sql.freedb.tech:3306/freedb_rpr baza";
+        String username="freedb_abuturovic1";
+        String pass = System.getenv("DB_PASSWORD");
+        connection = DriverManager.getConnection(url,username,pass);
+        CustomerImpl customerDAO = new CustomerImpl();
+        customerDAO.getAll();
+
+        return listData;
+    }
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
