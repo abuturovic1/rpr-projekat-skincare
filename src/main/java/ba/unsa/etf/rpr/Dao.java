aeb38dd9ddb9e69b030e33cc1d8f64562671cd50
Amina <abuturovic1@etf.unsa.ba>
2023-08-30 01:23:13 +0200

    added a user defined exception

--- src/main/java/ba/unsa/etf/rpr/Dao.java
+++ src/main/java/ba/unsa/etf/rpr/Dao.java
@@ -1,13 +1,15 @@
 package ba.unsa.etf.rpr;
 
+import ba.unsa.etf.rpr.exceptions.ReservationException;
+
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 public interface Dao <T>{
-    ArrayList<T> get(int id) ;
+    ArrayList<T> get(int id) throws ReservationException;
 
-    List<T> getAll();
+    List<T> getAll() throws ReservationException;
     void save(T t);
 
     void update(T t);
