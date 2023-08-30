13456abac821127a614667644a9b4899f075f34c
Amina <abuturovic1@etf.unsa.ba>
2023-08-31 00:39:10 +0200

    added javadoc

--- src/main/java/ba/unsa/etf/rpr/dao/TreatmentDAO.java
+++ src/main/java/ba/unsa/etf/rpr/dao/TreatmentDAO.java
@@ -4,6 +4,9 @@ import ba.unsa.etf.rpr.Treatment;
 
 import java.util.List;
 
+/**
+ * This interface defines data access operations for managing Treatment objects
+ */
 public interface TreatmentDAO extends Dao<Treatment> {
 List<Treatment>getbyTreatmentName(String name);
 
--- src/main/java/ba/unsa/etf/rpr/exceptions/ReservationException.java
+++ src/main/java/ba/unsa/etf/rpr/exceptions/ReservationException.java
@@ -1,5 +1,8 @@
 package ba.unsa.etf.rpr.exceptions;
 
+/**
+ * A user defined class for handling exceptions related to reservations.
+ */
 public class ReservationException extends Exception{
     public ReservationException(String mess,Exception cause){
         super(mess,cause);
