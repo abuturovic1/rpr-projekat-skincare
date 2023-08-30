bea9dc9e41558f5e2d32ee958104fffa991b057c
Amina <abuturovic1@etf.unsa.ba>
2023-08-30 23:35:27 +0200

    added javadoc

--- src/main/java/ba/unsa/etf/rpr/Reservation.java
+++ src/main/java/ba/unsa/etf/rpr/Reservation.java
@@ -3,11 +3,23 @@ import java.sql.Time;
 import java.time.LocalTime;
 import java.util.*;
 
-public class Reservation {  //javabean class
+/**
+ * The Reservation class is regular Java Bean class and represents a reservation made by customer for a treatment
+ */
+public class Reservation {
     private int reservationID,customerID,treatmentID;
     private String status,reservationDate,time;
 
-public Reservation(int reservationID,int customerID,int treatmentID,String reservationDate,String time,String status){
+    /**
+     * A constructor which constructs a new Reservation object with the provided information
+     * @param reservationID unique ID of the reservation
+     * @param customerID the ID of the customer making the reservation
+     * @param treatmentID the ID of the treatment being reserved
+     * @param reservationDate the date of the reservation
+     * @param time the time of the reservation
+     * @param status the status of the reservation
+     */
+    public Reservation(int reservationID,int customerID,int treatmentID,String reservationDate,String time,String status){
     this.reservationID=reservationID;
     this.customerID=customerID;
     this.treatmentID=treatmentID;
@@ -16,7 +28,11 @@ public Reservation(int reservationID,int customerID,int treatmentID,String reser
     this.status=status;
 
 }
-public Reservation(){}
+
+    /**
+     * Empty Constructor which constucts new empty Reservation object
+     */
+    public Reservation(){}
     public int getReservationID() {
         return reservationID;
     }
