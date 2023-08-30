7c0cda7a153f0726304614cf3298f399f7ba76a7
Amina <abuturovic1@etf.unsa.ba>
2023-08-30 23:49:42 +0200

    added javadoc

--- src/main/java/ba/unsa/etf/rpr/dao/ReservationDAO.java
+++ src/main/java/ba/unsa/etf/rpr/dao/ReservationDAO.java
@@ -5,7 +5,15 @@ import ba.unsa.etf.rpr.Reservation;
 
 import java.util.List;
 
+/**
+ * Defines methods for accessing and managing reservation data
+ */
 public interface ReservationDAO extends Dao<Reservation> {
 
+    /**
+     * Checks if a reservation date is already taken
+     * @param reservationDate the reservation date to check for availability
+     * @return it is true if the reservation date is already taken , otherwise false
+     */
     boolean isReservationDateTaken(String reservationDate);
 }
